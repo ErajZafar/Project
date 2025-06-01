6e4434803046d78a23a0685018b9360b52f812fc
ErajZafar <erajzafar2010@hotmail.com>
2025-06-01 22:31:52 +0100

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,23 +1,7 @@
-# ------------------------------------------------------------------
-# Copyright (c) 2020 PyInstaller Development Team.
 #
-# This file is distributed under the terms of the GNU General Public
-# License (version 2.0 or later).
+# SPDX-License-Identifier: BSD-2-Clause
+# Copyright 2013-2022, John McNamara, jmcnamara@cpan.org
 #
-# The full license is available in LICENSE.GPL.txt, distributed with
-# this software.
-#
-# SPDX-License-Identifier: GPL-2.0-or-later
-# ------------------------------------------------------------------
-import os
-from . import stdhooks
-from . import rthooks
-_FILE_DIR = os.path.dirname(__file__)
-
-
-def get_hook_dirs():
-    return [
-        *stdhooks.get_hook_dirs(),
-        *rthooks.get_hook_dirs(),
-        _FILE_DIR  # pre_* hooks
-    ]
+__version__ = '3.0.3'
+__VERSION__ = __version__
+from .workbook import Workbook
