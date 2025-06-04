b170bc754f21911bd4a8b85d0cd9200967470fa0
ErajZafar <erajzafar2010@hotmail.com>
2025-06-04 23:58:02 +0100

    command
     project\the-url-god\the-url-god\thGeo\Lib\site-packages\setuptools\command

--- __init__.py
+++ __init__.py
@@ -1,26 +1,8 @@
-# This file is dual licensed under the terms of the Apache License, Version
-# 2.0, and the BSD License. See the LICENSE file in the root of this repository
-# for complete details.
-from __future__ import absolute_import, division, print_function
+from distutils.command.bdist import bdist
+import sys
 
-from .__about__ import (
-    __author__,
-    __copyright__,
-    __email__,
-    __license__,
-    __summary__,
-    __title__,
-    __uri__,
-    __version__,
-)
+if 'egg' not in bdist.format_commands:
+    bdist.format_command['egg'] = ('bdist_egg', "Python .egg file")
+    bdist.format_commands.append('egg')
 
-__all__ = [
-    "__title__",
-    "__summary__",
-    "__uri__",
-    "__version__",
-    "__author__",
-    "__email__",
-    "__license__",
-    "__copyright__",
-]
+del bdist, sys
